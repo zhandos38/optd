6e37f906fa6d3c2cbfc4768b3c747db18d3b4c62
jake47 <zhandos38@gmail.com>
2020-12-17 11:41:36 +0600

    Header fixes

--- views/layouts/_header.php
+++ views/layouts/_header.php
@@ -1,18 +1,24 @@
 <header class="header">
     <div class="container" style="display: flex; justify-content: space-between">
-        <div>
-            <img class="header__symbol" src="/img/symbol.png" alt="">
-        </div>
-        <div class="header__content">
-            <div class="header__name">
-                <?= Yii::t('site', '"Управление здравохранения города Шымкент"') ?>
-            </div>
-            <div class="header__name">
-                <?= Yii::t('site', 'Добро пожаловать в ГКП на ПХВ "Городская поликлиника №3"') ?>
+        <div class="row">
+            <div class="col-md-2">
+                <img class="header__symbol" src="/img/symbol.png" alt="">
             </div>
         </div>
-        <div class="header__lang">
-            <?= $this->render('select-language') ?>
+        <div class="col-md-7">
+            <div class="header__content">
+                <div class="header__name">
+                    <?= Yii::t('site', '"Управление здравохранения города Шымкент"') ?>
+                </div>
+                <div class="header__name">
+                    <?= Yii::t('site', 'Добро пожаловать в ГКП на ПХВ "Городская поликлиника №3"') ?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="header__lang pull-right">
+                <?= $this->render('select-language') ?>
+            </div>
         </div>
     </div>
 </header>
