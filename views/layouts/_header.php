88ff94a35e8131f6162a246ef945acad91b95d21
zhandos38 <zhandos38@gmail.com>
2020-12-29 11:56:38 +0600

    Whatsapp fixes

--- views/layouts/_header.php
+++ views/layouts/_header.php
@@ -22,3 +22,8 @@
         </div>
     </div>
 </header>
+<div class="chat-box">
+    <button onclick="window.location.href='https://wa.me/77075539798'" type="button" id="whats-openPopup" class="whatsapp-button">
+        <img class="icon-whatsapp" src="https://image.flaticon.com/icons/svg/134/134937.svg">
+    </button>
+</div>
