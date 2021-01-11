75b4fe6608c285c47b2869bdcdf7547d5b63fd9d
jake47 <zhandos38@gmail.com>
2021-01-11 22:37:07 +0600

    Direction fixes

--- views/site/direction.php
+++ views/site/direction.php
@@ -11,16 +11,58 @@
     <div>
         <div class="row">
             <div class="col-md-12">
-                <img src="/img/director.png" alt="director">
+                <div class="card text-center">
+                    <img class="card-img-top" style="width: 30%" src="/img/director.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Мауленов Жаксылык Оразбайулы
+                        </h5>
+                        <p class="card-text">
+                            Директор городского онкологического центра
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row" style="padding-top: 40px;">
+            <div class="col-md-4">
+                <div class="card" style="text-align: center">
+                    <img class="card-img-top" style="width: 80%" src="/img/zam1.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Жұмағали Дәулетбек Жұманұлы
+                        </h5>
+                        <p class="card-text">
+                            Заместитель директора по организационно-методической  работе
+                        </p>
+                    </div>
+                </div>
             </div>
             <div class="col-md-4">
-                <img class="w-100" src="/img/zam1.jpg" alt="zam1">
+                <div class="card" style="text-align: center">
+                    <img class="card-img-top" style="width: 80%" src="/img/zam1.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Рахманов Динмухамед Абдикаимович
+                        </h5>
+                        <p class="card-text">
+                            Заместитель директора по качеству
+                        </p>
+                    </div>
+                </div>
             </div>
             <div class="col-md-4">
-                <img src="/img/zam2.jpg" alt="zam2">
-            </div>
-            <div class="col-md-4">
-                <img src="/img/zam2.jpg" alt="zam3">
+                <div class="card" style="text-align: center">
+                    <img class="card-img-top" style="width: 80%" src="/img/zam1.png" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            Исмаилов Самат Тастемирович
+                        </h5>
+                        <p class="card-text">
+                            Заместитель директора по лечебной части
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
