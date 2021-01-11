910026216d8538f53d3b23f6fc28e77246b3c59d
jake47 <zhandos38@gmail.com>
2021-01-11 21:42:16 +0600

    Footer support fixes

--- views/layouts/_footer.php
+++ views/layouts/_footer.php
@@ -1,5 +1,6 @@
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">Powered by <i class="fa fa-heart"></i> <a href="http://itbgroup.kz">Веб-студия IT Business Group</a></p>
+        <p class="pull-left"><?= Yii::t('site', 'Служба поддержки пациентов') ?>: <a href="tel:77252361113"> +7 (7252) 36-11-13</a></p>
+        <p class="pull-right">Powered by <i class="fa fa-heart" style="color: red"></i> <a href="http://itbgroup.kz">Веб-студия IT Business Group</a></p>
     </div>
 </footer>
