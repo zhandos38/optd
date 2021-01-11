3a20dbeac62f2d97bd4318424ccf2ceb6eab31bc
jake47 <zhandos38@gmail.com>
2021-01-11 21:35:54 +0600

    Other fixes

--- views/site/index.php
+++ views/site/index.php
@@ -20,13 +20,6 @@ $this->title = 'Главная старница';
         </a>
     </div>
     <div class="main-menu__item-wrapper">
-        <a href="<?= \yii\helpers\Url::to(['site/map']) ?>" class="main-menu__item-link">
-            <div class="main-menu__item">
-                <?= Yii::t('site', 'Расположение кабинетов') ?>
-            </div>
-        </a>
-    </div>
-    <div class="main-menu__item-wrapper">
         <a href="<?= \yii\helpers\Url::to(['site/support']) ?>" class="main-menu__item-link">
             <div class="main-menu__item">
                 <?= Yii::t('site', 'Служба поддержки пациентов') ?>
