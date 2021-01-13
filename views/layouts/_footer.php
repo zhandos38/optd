3c3a1a064fff4db0e01f12f839bcd10897813438
jake47 <zhandos38@gmail.com>
2021-01-13 21:57:14 +0600

    Main fixes

--- views/layouts/_footer.php
+++ views/layouts/_footer.php
@@ -1,5 +1,9 @@
 <footer class="footer">
     <div class="container">
-        <p class="pull-left" style="font-weight: bold"><?= Yii::t('site', 'Служба поддержки пациентов') ?>: <a href="tel:77252361113"> +7 (7252) 36-11-13</a></p>
+        <p class="pull-left" style="font-weight: bold"><?= Yii::t('site', 'Служба поддержки пациентов') ?>: <a href="tel:77252361113"> +7 (7252) 36-11-13</a>
+            <br>
+            <?= Yii::t('site', 'Номера доверия') ?>: <a href="tel:393347"><i class="fa fa-phone"></i> 39-33-47</a>, <i class="fa fa-mobile"></i> 1430, 1424, 1406, 109
+        </p>
+        <p class="pull-right studio-text">Powered by <i class="fa fa-heart" style="color: red"></i> <a href="http://itbgroup.kz">Веб-студия IT Business Group</a></p>
     </div>
 </footer>
